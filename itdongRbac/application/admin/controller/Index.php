64c8baa0dafdfd0e1a8e9174b439f752f5b80b6b
zhaoxudong <7879937@qq.com>
2018-05-10 18:18:02 +0800

    commit 201805101817 left side bar and right content.

--- itdongRbac/application/admin/controller/Index.php
+++ itdongRbac/application/admin/controller/Index.php
@@ -2,11 +2,24 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
     public function index()
     {
+    	$menu = [];
+    	$auth_rule_list = Db::name('auth_rule')->where("status",1)->order(["id"=>"ASC"])->select();
+		
+    	$menu = array2tree($auth_rule_list);
+
+        $this->assign('menu', $menu);
+
         return $this->fetch();
+    }
+
+    public function test()
+    {
+		return $this->fetch();
     }
 }
